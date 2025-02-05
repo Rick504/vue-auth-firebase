924b0517b61986181b706331784ca98b0ef7b553
Rick Rios <rick.wes1@gmail.com>
2025-02-05 17:19:48 -0300

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBVpzhefkmo5iZ2yOIlqtSqhXTi-NqTLEc',
-  authDomain: 'first-fire-20ce0.firebaseapp.com',
-  projectId: 'first-fire-20ce0',
-  storageBucket: 'first-fire-20ce0.firebasestorage.app',
-  messagingSenderId: '879858188385',
-  appId: '1:879858188385:web:15bae335fd5451afe22a20',
-  measurementId: 'G-H1KQGE1BY9',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
