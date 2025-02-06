32bf5239d425ee4b282985c6b33e8e5a545f7135
Rick Rios <rick.wes1@gmail.com>
2025-02-05 23:50:08 -0300

    feat: set token in cookies

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,16 @@
 import http from './config/axios'
+import Cookies from 'js-cookie'
 
 class AuthService {
-  public async login(email: string, password: string){
+  public async login(email: string, password: string) {
     try {
       const response = await http.post('/login', { email, password })
+      const token = response.data?.token
+
+      if (token) {
+        Cookies.set('x-access-token', token, { expires: 7, secure: true })
+      }
+
       return response.data
     } catch (error) {
       console.error('Erro ao fazer login:', error)
@@ -13,4 +20,3 @@ class AuthService {
 }
 
 export default AuthService
-
