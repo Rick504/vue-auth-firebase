93d92f421217212cf3fb0db9cc6c05e6fc87e8af
Rick Rios <rick.wes1@gmail.com>
2025-02-10 23:00:24 -0300

    feat: create form register

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="LoginForm" class="container d-flex justify-content-center align-items-center min-vh-100">
     <div class="card p-4 shadow-lg" style="max-width: 400px; width: 100%">
-      <h2 class="text-center mb-3 ">Login</h2>
-      <LoginForm />
+      <h2 class="text-center mb-3 gap-5 ">
+        <button @click="toggleLoginVisibility(true)" class="btn border">Entrar</button>
+        <button @click="toggleLoginVisibility(false)" class="btn border">Registro</button>
+      </h2>
+      <LoginForm v-if="showLogin" />
+      <RegisterForm v-else />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import LoginForm from './components/LoginForm.vue'
+import RegisterForm from './components/RegisterForm.vue'
+
+const showLogin = ref(true)
+
+const toggleLoginVisibility = (value: boolean) => {
+  showLogin.value = value
+}
 
 </script>
 
---
+++ src/views/Login/components/RegisterForm.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <form @submit.prevent="register">
+      <div class="mb-3">
+        <label for="userName" class="form-label">Nome</label>
+        <input
+          id="userName"
+          v-model="userName"
+          type="userName"
+          class="form-control"
+          placeholder="Digite seu e-mail"
+          required
+        />
+      </div>
+      <div class="mt-3" v-if="textPositions.errorUserName">
+        <p class="text-danger">{{ textPositions.errorUserName }}</p>
+      </div>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">E-mail</label>
+        <input
+          id="email"
+          v-model="email"
+          type="email"
+          class="form-control"
+          placeholder="Digite seu e-mail"
+          required
+        />
+      </div>
+      <div class="mt-3" v-if="textPositions.errorEmail">
+        <p class="text-danger">{{ textPositions.errorEmail }}</p>
+      </div>
+
+      <div class="mb-3 position-relative">
+        <label for="password" class="form-label">Senha</label>
+        <div class="input-group">
+          <input
+            id="password"
+            type="password"
+            v-model="password"
+            class="form-control"
+            placeholder="Digite sua senha"
+            required
+          />
+        </div>
+        <div class="mt-3" v-if="textPositions.errorPassword">
+          <p class="text-danger">{{ textPositions.errorPassword }}</p>
+        </div>
+      </div>
+
+      <div class="mb-3 position-relative">
+        <label for="confirmPassword" class="form-label">Confirme sua senha</label>
+        <div class="input-group">
+          <input
+            id="confirmPassword"
+            type="confirmPassword"
+            v-model="confirmPassword"
+            class="form-control"
+            placeholder="Digite sua senha"
+            required
+          />
+        </div>
+        <div class="mt-3" v-if="textPositions.errorConfirmPassword">
+          <p class="text-danger">{{ textPositions.errorConfirmPassword }}</p>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <button type="submit" class="btn btn-green w-100">Registrar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import validator from 'validator'
+// import { useRouter } from 'vue-router'
+// import UserService from '../../../services/UserService'
+
+import { useStore } from '../../../stores/index'
+const store = useStore()
+
+const userName = ref('')
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+
+const textPositions = ref({
+  errorUserName: '',
+  errorEmail: '',
+  errorConfirmPassword: '',
+})
+
+function clearErrorsForm() {
+  setError('errorUserName', '')
+  setError('errorEmail', '')
+  setError('errorConfirmPassword', '')
+}
+
+function validateMinLength(field: string, value: string, min: number) {
+  if (!isMinLength(value, min)) {
+    setError(field, `Campo deve ter pelo menos ${min} caracteres.`)
+    return false
+  }
+  return true
+}
+
+function validateUserName(value: string) {
+  const userNameRegex = /^[^\d]*$/
+  if (!userNameRegex.test(value)) {
+    setError('errorUserName', 'Nome de usuário não pode conter números.')
+    return false
+  }
+  return true
+}
+
+const isEmailValid = (email: string) => {
+  return validator.isEmail(email)
+}
+
+const isMinLength = (value: string, min: number) => {
+  return validator.isLength(value, { min })
+}
+
+const setError = (position: string, text: string) => {
+  textPositions.value[position] = text
+}
+
+const isLoader = (value: boolean) => {
+  return (store.loader = value)
+}
+
+const register = async () => {
+  isLoader(true)
+
+  clearErrorsForm()
+
+  // Validação de nome de usuário (mínimo de 6 caracteres e sem números)
+  if (!validateMinLength('errorUserName', userName.value, 6) || !validateUserName(userName.value)) {
+    isLoader(false)
+    return
+  }
+
+  // Validação de e-mail
+  if (!isEmailValid(email.value)) {
+    setError('errorEmail', 'Por favor, insira um e-mail válido.')
+    isLoader(false)
+    return
+  }
+
+  // Validação de senhas iguais
+  if (password.value !== confirmPassword.value) {
+    setError('errorConfirmPassword', 'As senhas não são iguais.')
+    isLoader(false)
+    return
+  }
+
+  // Validação de senha mínima
+  if (!validateMinLength('errorPassword', password.value, 6) || !validateMinLength('errorConfirmPassword', confirmPassword.value, 6)) {
+    isLoader(false)
+    return
+  }
+
+  try {
+    if (userName.value && email.value && password.value) {
+      const user = {
+        name: userName.value,
+        email: email.value,
+        password: password.value,
+      }
+      clearErrorsForm()
+      console.log(user)
+      isLoader(false)
+    }
+  } catch {
+    isLoader(false)
+  }
+}
+
+</script>
