f42658d5255557d10c08f765c68c49911a38801b
Rick Rios <rick.wes1@gmail.com>
2025-02-23 16:17:38 -0300

    feat: ignore type error

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -92,10 +92,11 @@ const loginWithGoogle = async () => {
       isLoader(false)
     }
   } catch (error){
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     textErrorLoginGoogle.value = error.response.data.error
     isLoader(false)
     errorLoginGoole.value = true
-    console.error('Erro ao autenticar com Google:')
   }
 
 }
